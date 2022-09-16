987c09b3a17dc530b974d52f28d5f22efc9d8bba
jefferson shibuya <Jefferson Shibuya>
2022-09-16 11:17:20 -0300

    Added tailwindcss, configured layout and created header sidebar and footer initial components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,5 +22,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0,
     'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 0,
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
+import './index.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="flex flex-row min-h-screen bg-gray-100 text-gray-800">
+      <Sidebar />
+      <main className="main flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
+        <Header />
+        <div className="main-content flex flex-col flex-grow p-2">
+          {/* <h1 className="font-bold text-2xl text-gray-700">Dashboard</h1> */}
+
+          <div className="flex flex-col flex-grow p-2 bg-white rounded">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </div>
+        {/* <Footer /> */}
+      </main>
+    </div>
   );
 }
 
